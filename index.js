1dd921510354da1e30c48edc87cd0691b0b96828
justm3sunny <justaskcoding76@gmail.com>
2025-09-04 14:23:56 +0530

    ai setup

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 #!usr/bin/env node
 
-import {program} from 'commander'
+import { program } from 'commander'
+import { GoogleGenAI } from "@google/genai";
 
-program 
+const prompt = "this is an test prompt"
+program
     .version("0.0.1")
     .description("a simple cli agent tool")
-    .option("--v --versbose","set to versbose mode",false)
-    .option("--apikey <type>","set gemini api key").action(async (input) => {
-        console.log("recived :",input)
+    .option("--v --versbose", "set to versbose mode", false)
+    .option("--apikey <type>", "set gemini api key")
+    .action(async (input) => {
+        const ai = new GoogleGenAI({ apiKey: input.apiKey });
+        if (input.verbose) {
+            console.log('genai initalize')
+        }
+        const response = await ai.models.generateContent({
+            model: "gemini-2.5-flash-lite"
+            , contents: prompt
+        })
+        console.log(response.text);
     })
-    program.parseAsync(process.argv)
+program.parseAsync(process.argv)
