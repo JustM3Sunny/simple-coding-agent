f9399e4be9f5057c1f7c74fbd6c06c5fa62ebb54
justm3sunny <justaskcoding76@gmail.com>
2025-09-04 14:42:33 +0530

    add histry fnc

--- index.js
+++ index.js
@@ -6,13 +6,34 @@ import readline from 'readline'
 import { stdin, stdout } from 'process';
 const prompt = "this is an test prompt"
 
-async function startIntractiveLoop(ai , verbose) {
+async function startIntractiveLoop(ai, verbose) {
+    let history = [];
+
     const rl = readline.createInterface({
-        input:stdin,
-        output:stdout,
-        prompt:'sia > '
+        input: stdin,
+        output: stdout,
+        prompt: 'sia > '
     })
     rl.prompt()
+    rl.on('line', async (input) => {
+        const trimmed = input.trim()
+        if (input == "exit") {
+            rl.close()
+            console.log("Thanks for Using! Made by Dev❤️ For Devs.")
+            return
+        } if (trimmed) {
+            //add user msg and conv to history 
+            history.push({ role: 'user', parts: [{ text: trimmed }] })
+
+            const response = await ai.models.generateContent({
+                model: "gemini-2.5-flash-lite"
+                , contents: history
+            })
+            console.log(response.text);
+            history.push({role:"model",parts:[{text:response.text}]})
+        }
+    })
+
 }
 program
     .version("0.0.1")
@@ -24,11 +45,7 @@ program
         if (input.verbose) {
             console.log('genai initalize')
         }
-        // const response = await ai.models.generateContent({
-        //     model: "gemini-2.5-flash-lite"
-        //     , contents: prompt
-        // })
-        // console.log(response.text);
-        startIntractiveLoop(ai,input.verbose)
+
+        startIntractiveLoop(ai, input.verbose)
     })
 program.parseAsync(process.argv)
